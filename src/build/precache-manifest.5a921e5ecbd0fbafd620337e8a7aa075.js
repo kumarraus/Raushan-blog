a5d8b94fcbff5e9cacba03fe268428ce54fc6b04
Raushan Kumar <raushan.kumar@sap.com>
2019-12-30 20:31:46 +0530

    First Commit

---
+++ src/build/precache-manifest.5a921e5ecbd0fbafd620337e8a7aa075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d726b672320da794b495f00f19d616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f1c71b27110bbf47eee",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0224531fb1c0e7486404",
+    "url": "/static/js/2.276efbdf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.276efbdf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f1c71b27110bbf47eee",
+    "url": "/static/js/main.eae36923.chunk.js"
+  },
+  {
+    "revision": "9f2f2cb3ed19f94b6ffe",
+    "url": "/static/js/runtime-main.86b77349.js"
+  }
+]);
